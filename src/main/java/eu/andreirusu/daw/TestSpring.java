f6e2439e4b6346910f19e0784e428599b2c82379
Andrei Rusu <andrei.rusu.teach@gmail.com>
2020-02-28 00:09:01 +0200

    S-a efectuat primul pas de utilizare a Spring-ului

---
+++ src/main/java/eu/andreirusu/daw/TestSpring.java
@@ -0,0 +1,19 @@
+package eu.andreirusu.daw;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+        
+		Music music = context.getBean("musicBean", Music.class);
+        // Oricum inca cream obiecte direct din aplicatie, deci
+		// inca avem o cuplare a obiectelor ...
+		MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
+
+        context.close();
+	}
+
+}
